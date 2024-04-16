09a67b5641933823de5d8c9bf530959c724c5b3e
Ayush Vanvary <ayush.vanvary10@gmail.com>
2024-04-16 17:29:34 +0530

    Database configured

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,21 +2,16 @@
 Sample tests
 """
 
-from django.test import SimpleTestCase
 
+from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    
     def test_add_numbers(self):
-        
-        res= calc.add(5,6)
-        
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
-        
-    
+
     def test_Subtract_numbers(self):
-        
-        res = calc.subtract(10,15)
-        
-        self.assertEqual(res,-5)
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
