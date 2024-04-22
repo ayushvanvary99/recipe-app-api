01ec562a99e70bb942e78ea1ed093a5214ed4e4d
Ayush Vanvary <ayush.vanvary10@gmail.com>
2024-04-23 01:09:10 +0530

    Update custom user add through django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,47 @@
-from django.contrib import admin # noqa
+"""
+Django Admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permission'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fieldsets = ['last_login']
+    add_fields = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_admin',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
